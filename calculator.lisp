dded4a533afacbc15eb18ef304c100b841625035
Aleksandr Vinokurov <aleksandr.vin@gmail.com>
2011-10-23 03:35:06 +0400

    Parse function plus minor modifications.
     * Added declarations to functions.
     * Changed 'exp rule in grammar to help parse function.
     * Faked 'let-op rule.
     * Removed +lexems+ list due to its worthlessness.
     * Added 'product predicates to types and funcs of the grammar.
     * define-type and define-func made less leaky: they return the name of the object they operate on.
     * Added skipped-lexem-p predicate to check if the subject predicate must be skipped.
     * Fixed check-integer function: to be sure the whole value is parsed into integer and that the result is an integer.
     * Added package for interning variables of the expressions when parsing.
     * Added read-lexem-chars: for reading a succeding lexem and returning it as a list of characters.
     * Added lexem-divider-p predicate to test for the character to be a dividing lexem.
     * Fixing string-to-lexems: supporting strings among list lexems.
     * Added test for string-to-lexems.
     * Added parse-with-rule: the main parsing dispatcher.
     * Added parse-with-list-rule: a parser for the rule-value lists.
     * Added test for parse-with-exp-rule, which is a renaming function for testing purposes.
     * Updated test for the parse function.

--- calculator.lisp
+++ calculator.lisp
@@ -54,6 +54,9 @@
 
 ;;; Testing framework
 
+;; For more details on self-testing trace next functions:
+;(trace testing-function test-case)
+
 (defvar *tested-function* #'identity
   "Variable for binding tested functions.")
 
@@ -64,10 +67,8 @@
 (defun test-case (case)
   "Check one test case."
   (destructuring-bind (in => out) case
+    (declare (ignore =>))
     (equal out (testing-function in))))
-
-(trace testing-function
-       test-case)
 
 (defun test (function-name test-name test-cases)
   "Test function across test-cases."
@@ -78,12 +79,13 @@
 	(format *trace-output* "~&Test OK.~%")
 	(format *trace-output* "~&Test FAILED.~%"))))
 
-(defmacro make-test (for function-name acros test-cases &body test-cases)
+(defmacro make-test (for function-name acros test-cases &body cases)
   "Create a lambda for testing function-name acros test-cases."
+  (declare (ignore for acros test-cases))
   `(function (lambda ()
       (test ',function-name
 	    (format nil "~a function test" ',function-name)
-	    ,@test-cases))))
+	    ,@cases))))
 
 (funcall
  (make-test for string acros test-cases
@@ -106,19 +108,14 @@
 ;;; Grammar definition
 
 (defvar +grammar+
-  '((exp         => num var func let)
+  '((exp         => func let var num)
     (func        => (func-name "(" exp "," exp ")"))
     (let         => (let-op "(" var "," exp "," body))
     (body        => exp)
-    (func-name   => add-func sub-func mult-func div-func))
+    (func-name   => add-func sub-func mult-func div-func)
+    ;; let-op fake
+    (let-op      => "let-op"))
   "Grammar for the evaluation of the calculator input.")
-
-(defvar +lexems+
-  '(" " "(" ")" ","
-    num var
-    add-func sub-func mult-func div-func
-    let-op)
-  "Lexems of the grammar.")
 
 (defvar +skipped-lexems+
   '(" ")
@@ -126,10 +123,16 @@
 
 ;;; Grammar rules macros
 
-(defmacro define-type (type-name &key check-value-predicate)
+(defmacro define-type (type-name &key check-value-predicate
+		       product-predicate)
   "Define grammar type with the predicate to check a value."
   `(setf (get ',type-name 'check-value)
-         ,check-value-predicate))
+         ,check-value-predicate)
+
+  `(setf (get ',type-name 'product)
+	 ,product-predicate)
+
+  type-name)
 
 (defmacro define-func (func-name &key name evaluating-func)
   `(progn
@@ -138,21 +141,27 @@
                (when (stringp value)
                  (string= ,name value))))
 
+     (setf (get ',func-name 'product)
+	   #'(lambda (value)
+	       (declare (ignore value))
+	       ',func-name))
+
      (setf (get ',func-name 'evaluating-func)
-           ,evaluating-func)))
+           ,evaluating-func))
+
+  func-name)
 
 ;;; Grammar rules definitions
+
+(defun skipped-lexem-p (lexem)
+  "Return T if the lexem must be skipped."
+  (find lexem +skipped-lexems+ :test #'string=))
 
 (defvar Integer.MIN_VALUE -100
   "Minimum value of the 'num' expression.")
 
 (defvar Integer.MAX_VALUE 100
   "Maximum value of the 'num' expression.")
-
-;; Value checking lambda for the 'num' expression
-(define-type num
-    :check-value-predicate
-  #'check-integer)
 
 (defun check-integer (number-or-string)
   "Return T if number-or-string is an integer in allowed
@@ -161,10 +170,20 @@
 	 (num
 	  (typecase value
 	    (number value)
-	    (string (parse-integer value :junk-allowed t))
+	    (string
+	     (multiple-value-bind (int pos)
+		 (parse-integer value :junk-allowed t)
+	       (when (= pos (length value))
+		 int)))
 	    (t nil))))
     (when num
-      (and (<= Integer.MIN_VALUE num Integer.MAX_VALUE)))))
+      (and (integerp num)
+		 (<= Integer.MIN_VALUE num Integer.MAX_VALUE)))))
+
+;; Value checking lambda for the 'num' expression
+(define-type num
+    :check-value-predicate #'check-integer
+    :product-predicate #'parse-integer)
 
 (funcall
  (make-test for check-integer acros test-cases
@@ -176,14 +195,19 @@
 
 ;(funcall (get 'num 'check-value) 14)
 
+;; Package for interning variables of the expressions
+(defpackage :interview.calculator.vars)
+
 ;; Value checking lambda for the 'var' expression
 (define-type var
     :check-value-predicate
   #'(lambda (value)
       (when (stringp value)
         (every #'(lambda (c)
-                   (or (char<= #\A c #\Z) (char<= #\a c #\z)))
-               value))))
+			 (or (char<= #\A c #\Z) (char<= #\a c #\z)))
+		     value)))
+  :product-predicate #'(lambda (value)
+			 (intern value :interview.calculator.vars)))
 
 ;(funcall (get 'var 'check-value) "abcS12")
 
@@ -240,11 +264,6 @@
   "Return a value of the grammar rule. The result is a list."
   (rest (rest rule)))
 
-;(get-rule-value (get-rule 'exp))
-
-(defun parse (input-string)
-  "Parse input-string using grammar rules. Return a tree.")
-
 (defun make-lexer (input-string)
   "Return a closure function that will return succeeding lexems."
   (let ((s (make-string-input-stream input-string)))
@@ -252,62 +271,111 @@
 	(read-lexem s nil))))
 
 (defun read-lexem (&optional stream &rest read-char-args)
-					;(apply #'read-char stream read-char-args))
+  "Return a succeding lexem from the stream. Result is a string."
+  (concatenate 'string (apply #'read-lexem-chars
+			      stream read-char-args)))
+
+(defun read-lexem-chars (&optional stream &rest read-char-args)
+  "Return a succeding lexem from the stream.
+   Result is a list of characters."
   (loop for ch = (apply #'read-char stream read-char-args)
      while ch
+     until (lexem-divider-p ch)
      collect ch into lexem
-     while (lexemp (concatenate 'string lexem))
-     finally (return lexem)))
+     finally (return
+	       (if (and (lexem-divider-p ch)
+			(null lexem))
+		   (progn
+		     (list ch))
+		   (progn
+		     (when ch
+		       (unread-char ch stream))
+		     lexem)))))
 
-(defun lexemp (lexem?)
-  "Return T if the string is a lexem."
-  (when (find-if #'(lambda (l)
-	       (cond ((stringp l) (string= l lexem?))
-		     ((symbolp l)
-		      (let ((check (get l 'check-value)))
-			(when check
-			  (funcall check lexem?))))
-		     (t nil)))
-	   +lexems+)
-    t))
+(defun lexem-divider-p (char)
+  "Return T if the char is a divider lexem."
+  (find char " (),"))
 
-(funcall
- (make-test for lexemp acros test-cases
-   `(("a" => t)
-     ("a b" => nil)
-     ("(" => t)
-     (" " => t)
-     (")" => t)
-     (,(format nil "~S" (1- Integer.MAX_VALUE)) => t)
-     (,(format nil "~S" (1+ Integer.MAX_VALUE)) => nil))))
-
-(with-input-from-string (s "abcd")
-  (read-lexem s nil))
-
-(defvar x (make-lexer "abcd"))
-(funcall x)
-
-(defun string-to-lexems (string)
-  "Convert string to lexem list."
-  (do* ((lexer (make-lexer string))
-	(lexem t (funcall lexer))
-	(lexems nil (cons lexem lexems)))
-       ((not lexem) (nreverse (rest lexems)))))
-
-; Alternative implementation via LOOP macro
 (defun string-to-lexems (string)
   "Convert string to lexem list."
   (loop with lexer = (make-lexer string)
      for lexem = (funcall lexer)
-     while lexem collect lexem))
+     while (string< "" lexem) collect lexem))
 
-(string-to-lexems "abcd")
+(funcall
+ (make-test for string-to-lexems acros test-cases
+   '(("1" => ("1"))
+     ("1 2 3" => ("1" " " "2" " " "3"))
+     ("1,2,3" => ("1" "," "2" "," "3"))
+     ("abc" => ("abc"))
+     ("a,b,-23" => ("a" "," "b" "," "-23"))
+     ("a-2" => ("a-2"))
+     ("  " => (" " " "))
+     ("ab(cd(ef)12,22" => ("ab" "(" "cd" "(" "ef" ")" "12" "," "22")))))
+
+(defun parse (input-string)
+  "Parse input-string using grammar rules. Return a tree."
+  (let* ((lexems (string-to-lexems input-string))
+	 (lexems (remove-if #'skipped-lexem-p lexems)))
+    (parse-with-rule 'exp lexems)))
+
+(defun parse-with-rule (rule lexems)
+  (etypecase rule
+    (string (when (string= rule (first lexems))
+	      (values '(t) #|(list (first lexems))|# (rest lexems))))
+    (list (multiple-value-bind (p l) (parse-with-list-rule rule lexems)
+	    (when p
+	      (values (list p) l))))
+    (symbol (let ((rv (get-rule-value (get-rule rule)))
+		  (cv (get rule 'check-value))
+		  (producer (get rule 'product))
+		  (lexem (first lexems)))
+	      (if (null rv)
+		  (if cv
+		      (let ((cv-result (funcall cv lexem)))
+			(when cv-result
+			  (values (list (funcall producer lexem))
+				  (rest lexems))))
+		      (error "No check-value function for ~A rule" rule))
+		  (loop for v in rv
+		       do (multiple-value-bind (p l) (parse-with-rule v lexems)
+			    (when p
+			      (return (values p l))))))))))
+
+(defun parse-with-exp-rule (lexems)
+  "Parse lexems list with the 'exp rule.
+   See parse-with-rule function."
+  (parse-with-rule 'exp lexems))
+
+(funcall
+ (make-test for parse-with-exp-rule acros test-cases
+   `((("a") => (,(intern "a" :interview.calculator.vars)))
+     (("1") => (1))
+     (("add" "(" "1" "," "2" ")") => ((add-func t 1 t 2 t))))))
+
+(defun parse-with-list-rule (list-rule lexems)
+  (declare (type list list-rule))
+  (unless (null list-rule)
+    (multiple-value-bind (hp hl) (parse-with-rule (first list-rule) lexems)
+      (if (null (rest list-rule))
+	  (values hp hl)
+	  (when hp
+	    (multiple-value-bind (rest-p rest-l)
+		(parse-with-list-rule (rest list-rule) hl)
+	      (when rest-p
+		(values (append hp rest-p) rest-l))))))))
+
+(parse "add(a, div(100,-10))")
 
 (funcall
  (make-test for parse acros test-cases
-   '(("1" => 1)
-     ("3" => 3)
-     ("add(1, 2)" => (add 1 2)))))
+   `(("1" => (1))
+     ("3" => (3))
+     ("abc" => (,(intern "abc" :interview.calculator.vars)))
+     ("add(1, 2)" => ((add-func t 1 t 2 t)))
+     ("div( s ,  add(2,1))"
+      => ((div-func t ,(intern "s" :interview.calculator.vars)
+		   t (add-func t 2 t 1 t) t))))))
 
 ;;; Interface
 
